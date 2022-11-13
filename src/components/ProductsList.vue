bf9f3eba341e93a64aff7f4f31c8b22648f9dec7
Elena Lorente <lorentedev@gmail.com>
2022-11-13 18:22:32 +0100

    Replace products in ProductsList with ProductCard template

--- src/components/ProductsList.vue
+++ src/components/ProductsList.vue
@@ -1,12 +1,28 @@
 <template>
   <section id="products-list">
-    <aside>
-      <h2>Category</h2>
-    </aside>
-    <section id="products-found">
-      <article v-for="product in products" :key="product._id">
-        {{ product.name }}
-      </article>
+    <section id="products-list-top">
+      <h1>
+        Photography / Premium Photos
+      </h1>
+      <section>
+        <label>Sort by
+          <select>
+            <option value="price">Price</option>
+            <option value="name">Name</option>
+          </select>
+        </label>
+      </section>
+    </section>
+    <section id="products-list-content">
+      <aside>
+        <h2>Category</h2>
+      </aside>
+      <section id="products-list-found">
+        <ProductCard
+          v-for="product in products"
+          :key="product._id"
+          :product="product" />
+      </section>
     </section>
   </section>
 </template>
@@ -14,13 +30,17 @@
 <script lang="ts">
 import { defineComponent, PropType } from 'vue'
 import Product from '../types/Product'
+import ProductCard from '../components/ProductCard.vue'
 
 export default defineComponent({
   name: 'ProductsList',
+  components: {
+    ProductCard
+  },
   props: {
     products: {
       required: true,
-      type: Array as PropType<Product>
+      type: Array as PropType<Product[]>
     }
   }
   // setup() {
@@ -30,8 +50,18 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
-#products-list {
+#products-list-content {
   display: flex;
   justify-content: space-between;
+}
+#products-list-top {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+#products-list-found {
+  display: flex;
+  align-content: flex-start;
+  flex-wrap: wrap;
 }
 </style>
