f1fffcae178b1979202198d85270d34d4d383776
Elena Lorente <lorentedev@gmail.com>
2022-11-16 18:01:35 +0100

    Add loading animation

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,14 +1,25 @@
 <template>
-  <div v-if="!$store.getters.loading">
+  <section v-if="!$store.getters.loading">
     <Header />
     <Separator />
     <FeaturedProduct />
     <Separator />
     <ProductsList :products="$store.getters.products" :paginationData="$store.getters.paginationData" />
-  </div>
-  <div v-else>
-    <span>Loading...</span>
-  </div>
+  </section>
+  <section
+    v-else
+    id="loading">
+    <div class="wave"></div>
+    <div class="wave"></div>
+    <div class="wave"></div>
+    <div class="wave"></div>
+    <div class="wave"></div>
+    <div class="wave"></div>
+    <div class="wave"></div>
+    <div class="wave"></div>
+    <div class="wave"></div>
+    <div class="wave"></div>
+  </section>
 </template>
 
 <script lang="ts">
@@ -68,5 +79,57 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
+#loading {
+  height: 80vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.wave {
+  width: 5px;
+  height: 100px;
+  background: linear-gradient(45deg, #000, #fff);
+  margin: 10px;
+  animation: wave 1s linear infinite;
+  border-radius: 20px;
+}
+.wave:nth-child(2) {
+  animation-delay: 0.1s;
+}
+.wave:nth-child(3) {
+  animation-delay: 0.2s;
+}
+.wave:nth-child(4) {
+  animation-delay: 0.3s;
+}
+.wave:nth-child(5) {
+  animation-delay: 0.4s;
+}
+.wave:nth-child(6) {
+  animation-delay: 0.5s;
+}
+.wave:nth-child(7) {
+  animation-delay: 0.6s;
+}
+.wave:nth-child(8) {
+  animation-delay: 0.7s;
+}
+.wave:nth-child(9) {
+  animation-delay: 0.8s;
+}
+.wave:nth-child(10) {
+  animation-delay: 0.9s;
+}
 
+@keyframes wave {
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1);
+  }
+  100% {
+    transform: scale(0);
+  }
+}
 </style>
